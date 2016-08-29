bc5c548b69632076f4c18ddb8bab05a7df568778
keithlei01 <keithlei01@gmail.com>
2016-08-29 15:46:23 +0800

    Hide themes layout when no theme

--- android-studio/BeautyPop/app/src/main/java/com/beautypop/fragment/ThemeFragment.java
+++ android-studio/BeautyPop/app/src/main/java/com/beautypop/fragment/ThemeFragment.java
@@ -40,22 +40,24 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class ThemeFragment extends TrackedFragment {
-	private LinearLayout themeImagesLayout;
-	private LinearLayout placeHolder;
-	int imageWidth,margin;
+	private LinearLayout themesLayout, trendsLayout, themeImagesLayout, placeHolder;
+	private int themeImageWidth, productImageWidth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 
 		View view = inflater.inflate(R.layout.theme_fragment, container, false);
-		themeImagesLayout = (LinearLayout)view. findViewById(R.id.themeImagesLayout);
+		themesLayout = (LinearLayout)view. findViewById(R.id.themesLayout);
+        trendsLayout = (LinearLayout)view. findViewById(R.id.trendsLayout);
+        themeImagesLayout = (LinearLayout)view. findViewById(R.id.themeImagesLayout);
 		placeHolder = (LinearLayout)view. findViewById(R.id.placeHolder);
 
 		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
 		params.addRule(RelativeLayout.BELOW,R.id.themeLabel);
 
-		imageWidth = (int) ((double) ViewUtil.getDisplayDimensions(getActivity()).width() / 3.5);  // fit around 3.5 items
+        themeImageWidth = (int) ((double) ViewUtil.getDisplayDimensions(getActivity()).width() / 3.5);  // fit around 3.5 items
+        productImageWidth = (int) ((double) ViewUtil.getDisplayDimensions(getActivity()).width() / 3.0);  // fit around 3.0 items
 
 		showThemes();
 		showTrends();
@@ -70,41 +72,43 @@ public class ThemeFragment extends TrackedFragment {
 
 	public void showThemes(){
 
-		themeImagesLayout.removeAllViews();
+        themesLayout.setVisibility(View.GONE);
 
-		int imageWidth = (int) ((double) ViewUtil.getDisplayDimensions(getActivity()).width() / 3.5);  // fit around 3.5 items
-		int padding = 10;
-		for (final CategoryVM vm : CategoryCache.getThemeCategories()) {
+        themeImagesLayout.removeAllViews();
+        if (CategoryCache.getThemeCategories().size() > 0) {
+            int padding = 10;
+            for (final CategoryVM vm : CategoryCache.getThemeCategories()) {
+                FrameLayout layout = new FrameLayout(getActivity());
+                layout.setPadding(padding, padding, padding, padding);
+                layout.setLayoutParams(new ViewGroup.LayoutParams(themeImageWidth, themeImageWidth));
 
-			FrameLayout layout = new FrameLayout(getActivity());
-            layout.setPadding(padding,padding,padding,padding);
-			layout.setLayoutParams(new ViewGroup.LayoutParams(imageWidth, imageWidth));
+                ImageView imageView = new ImageView(getActivity());
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(themeImageWidth, themeImageWidth));
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                ImageUtil.displayImage(vm.getThumbnail(), imageView);
+                layout.addView(imageView);
 
-			ImageView imageView = new ImageView(getActivity());
-			imageView.setLayoutParams(new ViewGroup.LayoutParams(imageWidth, imageWidth));
-			imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-			ImageUtil.displayImage(vm.getThumbnail(), imageView);
-			layout.addView(imageView);
+                TextView textView = new TextView(getActivity());
+                textView.setText(vm.getName());
+                textView.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
+                textView.setPadding(0, 0, 0, 10);
+                textView.setTextColor(Color.WHITE);
+                textView.setTextSize(13);
+                textView.setBackground(getActivity().getResources().getDrawable(R.drawable.gradient_bottom));
+                layout.addView(textView);
 
-			TextView textView = new TextView(getActivity());
-			textView.setText(vm.getName());
-			textView.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
-            textView.setPadding(0,0,0,10);
-			textView.setTextColor(Color.WHITE);
-            textView.setTextSize(13);
-			textView.setBackground(getActivity().getResources().getDrawable(R.drawable.gradient_bottom));
-			layout.addView(textView);
-
-			layout.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(getActivity(), ThemeActivity.class);
-					intent.putExtra(ViewUtil.BUNDLE_KEY_CATEGORY_ID, vm.getId());
-					startActivity(intent);
-				}
-			});
-			themeImagesLayout.addView(layout);
-		}
+                layout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), ThemeActivity.class);
+                        intent.putExtra(ViewUtil.BUNDLE_KEY_CATEGORY_ID, vm.getId());
+                        startActivity(intent);
+                    }
+                });
+                themeImagesLayout.addView(layout);
+            }
+            themesLayout.setVisibility(View.VISIBLE);
+        }
 	}
 
 	@Override
@@ -164,94 +168,94 @@ public class ThemeFragment extends TrackedFragment {
 			}
 
 		trendImageView.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(getActivity(), ThemeActivity.class);
-					intent.putExtra(ViewUtil.BUNDLE_KEY_CATEGORY_ID, vm.getId());
-					getActivity().startActivity(intent);
-				}
-			});
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ThemeActivity.class);
+                intent.putExtra(ViewUtil.BUNDLE_KEY_CATEGORY_ID, vm.getId());
+                getActivity().startActivity(intent);
+            }
+        });
 
 		}
 	}
 
-
 	public void getPopularProducts(CategoryVM categoryVM, final LinearLayout moreProductsImagesLayout, final View v){
 		ViewUtil.showSpinner(getActivity());
-		AppController.getApiService().getCategoryPopularFeed(0L,categoryVM.getId(), FeedFilter.ConditionType.ALL.name(),new Callback<List<PostVMLite>>() {
-			@Override
-			public void success(List<PostVMLite> postVMLites, Response response) {
-				for (final PostVMLite vm : postVMLites) {
+		AppController.getApiService().getCategoryPopularFeed(0L, categoryVM.getId(), FeedFilter.ConditionType.ALL.name(), new Callback<List<PostVMLite>>() {
+            @Override
+            public void success(List<PostVMLite> postVMLites, Response response) {
+                for (final PostVMLite vm : postVMLites) {
 
-					LinearLayout layout = new LinearLayout(getActivity());
-					layout.setOrientation(LinearLayout.VERTICAL);
-					LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-					params.setMargins(5,0,5,0);
-					layout.setLayoutParams(params);
-					ImageView imageView = new ImageView(getActivity());
-					imageView.setLayoutParams(new ViewGroup.LayoutParams(imageWidth, imageWidth));
-					imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    LinearLayout layout = new LinearLayout(getActivity());
+                    layout.setOrientation(LinearLayout.VERTICAL);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(5, 0, 5, 0);
+                    layout.setLayoutParams(params);
+                    ImageView imageView = new ImageView(getActivity());
+                    imageView.setLayoutParams(new ViewGroup.LayoutParams(productImageWidth, productImageWidth));
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
-					if (vm.getImages() != null && vm.getImages().length != 0) {
-						loadImage(vm.getImages()[0], imageView);
-					}
-					layout.addView(imageView);
+                    if (vm.getImages() != null && vm.getImages().length != 0) {
+                        loadImage(vm.getImages()[0], imageView);
+                    }
+                    layout.addView(imageView);
 
-					LinearLayout layout1 = new LinearLayout(getActivity());
-					layout1.setOrientation(LinearLayout.HORIZONTAL);
+                    LinearLayout layout1 = new LinearLayout(getActivity());
+                    layout1.setOrientation(LinearLayout.HORIZONTAL);
 
-					TextView originalPrice = new TextView(getActivity());
-					LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-					layoutParams.gravity = Gravity.CENTER;
-					layoutParams.setMargins(5,0,0,0);
+                    TextView originalPrice = new TextView(getActivity());
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    layoutParams.gravity = Gravity.CENTER;
+                    layoutParams.setMargins(5, 0, 0, 0);
 
-					layout1.setGravity(Gravity.CENTER_HORIZONTAL);
+                    layout1.setGravity(Gravity.CENTER_HORIZONTAL);
 
-					TextView priceText = new TextView(getActivity());
+                    TextView priceText = new TextView(getActivity());
 
-					if (vm.getOriginalPrice() > 0) {
+                    priceText.setText(ViewUtil.formatPrice(vm.getPrice()));
+                    priceText.setLayoutParams(layoutParams);
+                    priceText.setTextSize(13);
+                    priceText.setTypeface(null, Typeface.BOLD);
+                    priceText.setGravity(Gravity.CENTER);
+                    priceText.setTextColor(getActivity().getResources().getColor(R.color.light_gray));
+                    priceText.setPadding(5, 0, 5, 0);
+                    priceText.setGravity(Gravity.CENTER_HORIZONTAL);
+                    layout1.addView(priceText);
 
-						originalPrice.setText(ViewUtil.formatPrice(vm.getOriginalPrice()));
-						originalPrice.setGravity(Gravity.CENTER);
-						originalPrice.setTextSize(13);
-						originalPrice.setTypeface(null, Typeface.BOLD);
-						originalPrice.setTextColor(getActivity().getResources().getColor(R.color.light_gray));
-						layout1.addView(originalPrice);
-						originalPrice.setVisibility(View.VISIBLE);
-						originalPrice.setText(ViewUtil.formatPrice(vm.getOriginalPrice()));
-						ViewUtil.strikeText(originalPrice);
-					} else {
+                    if (vm.getOriginalPrice() > 0) {
+                        originalPrice.setText(ViewUtil.formatPrice(vm.getOriginalPrice()));
+                        originalPrice.setGravity(Gravity.CENTER);
+                        originalPrice.setTextSize(13);
+                        originalPrice.setTypeface(null, Typeface.NORMAL);
+                        originalPrice.setTextColor(getActivity().getResources().getColor(R.color.gray));
+                        originalPrice.setText(ViewUtil.formatPrice(vm.getOriginalPrice()));
+                        originalPrice.setPadding(5, 0, 5, 0);
+                        ViewUtil.strikeText(originalPrice);
+                        originalPrice.setVisibility(View.VISIBLE);
+                        layout1.addView(originalPrice);
+                    } else {
+                        originalPrice.setVisibility(View.GONE);
+                    }
 
-						priceText.setText(ViewUtil.formatPrice(vm.getPrice()));
-						priceText.setLayoutParams(layoutParams);
-						priceText.setTextSize(13);
-						priceText.setTypeface(null, Typeface.BOLD);
-						priceText.setGravity(Gravity.CENTER);
-						priceText.setTextColor(getActivity().getResources().getColor(R.color.green));
-						layout1.addView(priceText);
+                    layout1.setPadding(0,5,0,0);
+                    layout.addView(layout1);
 
-						originalPrice.setVisibility(View.GONE);
-						priceText.setGravity(Gravity.CENTER_HORIZONTAL);
-					}
+                    layout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ViewUtil.startProductActivity(getActivity(), vm.getId());
+                        }
+                    });
 
-					layout.addView(layout1);
+                    moreProductsImagesLayout.addView(layout);
+                    ViewUtil.stopSpinner(getActivity());
+                }
+            }
 
-					layout.setOnClickListener(new View.OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							ViewUtil.startProductActivity(getActivity(), vm.getId());
-						}
-					});
-
-					moreProductsImagesLayout.addView(layout);
-					ViewUtil.stopSpinner(getActivity());
-				}
-			}
-
-			@Override
-			public void failure(RetrofitError error) {
-				ViewUtil.stopSpinner(getActivity());
-			}
+            @Override
+            public void failure(RetrofitError error) {
+                ViewUtil.stopSpinner(getActivity());
+            }
 		});
 
 	}
